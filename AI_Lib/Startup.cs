0acf917bce232591b2d7933502b9bfd1c3841d3a
Arkadiusz_Zmijewski <arcun4@gmail.com>
2016-11-30 22:34:37 +0100

    Zmiana sposobu implementacji z java na .net

---
+++ AI_Lib/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AI_Lib.Startup))]
+namespace AI_Lib
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
